4cf195fa97a9f1b184a7fd660599b19bce63bedc
Christopher Sadler <sadlerlogic@gmail.com>
2022-02-01 18:34:50 -0600

    commit tree 7d24fabeb595dcf541b82ee4c223509efca81aeb

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood;
+
+mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
+
+
+
+
+
+
